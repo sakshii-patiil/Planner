ef63fcc6d022dad14768a7dc6e5d929ef2fe1e74
sakshii-patiil <67887279+sakshii-patiil@users.noreply.github.com>
2020-07-17 17:34:17 +0530

    Create after_signIn.php

---
+++ after_signIn.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create List</title>
+</head>
+ <link rel="stylesheet" href="about.css">
+<body>
+    <div class="top-div">
+        <div id="topnav" class="topnav">
+		   <a href="index.html"><button>Planner</button><label>.in</label></a>
+		   <div class="tab">
+		   <a href="createlist.html">Create List</a>
+		   <a href="#news">My List</a>
+		   </div>
+				<div class="top_menu">
+					<a href="#" class="menu">
+						<div class="one"></div>
+						<div class="two"></div>
+						<div class="three"></div>
+					<ul class="drop_down">
+						<li>Profile</li>
+						<li>Settings</li>
+						<li>Log Out</li>
+					</ul>
+				 </a>
+		 </div>
+   	    </div>
+
+		<div class="add_note">
+			<button><img class="plus" src="addNote.png" height="100px"</button>
+			<h4 style="font-family: Arial, Helvetica, sans-serif; font-size:25px;" >New</h4>
+			</div>
+		</div>
+	    
+    </div>
+    <div class="bottom-div">
+        <footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Planner<span>.in</span></h3>
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Blog</a>
+					·
+					<a href="#">About</a>
+				</p>
+
+				<p class="footer-company-name">Planner © 2020</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="facebook"><img src="facebook.png" alt=""></i></a>
+					<a href="#"><i class="instagram"><img src="instagram.png" alt=""></i></a>
+					<a href="#"><i class="twitter"><img src="twitter.png" alt=""></i></a>
+					<a href="#"><i class="github"><img src="linkedin.png" alt=""></i></a>
+
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p>Contact Us</p>
+
+				<form action="#" method="post">
+
+					<input type="text" name="email" placeholder="Email">
+					<textarea name="message" placeholder="Message"></textarea>
+					<button>Send</button>
+
+				</form>
+
+			</div>
+
+		</footer>
+    </div>
+</body>
+</html>
